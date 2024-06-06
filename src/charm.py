7d27a8d16ddecc113bf90c1bc59d34771e62df70
Ali Kelkawi <81743070+kelkawi-a@users.noreply.github.com>
2024-06-06 14:58:23 +0300

    Refactor charm and add update status hook (#7)
    * refactor charm and add update status hook

--- src/charm.py
+++ src/charm.py
@@ -9,10 +9,13 @@ from pathlib import Path
 
 import ops
 from botocore.exceptions import ClientError
-from charm_helpers import create_env
 from charms.data_platform_libs.v0.data_models import TypedCharmBase
 from charms.data_platform_libs.v0.database_requires import DatabaseRequires
 from charms.data_platform_libs.v0.s3 import S3Requirer
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
+from ops.pebble import CheckStatus
+
+from charm_helpers import create_env
 from literals import (
     BUCKET_CONFIGS,
     CONNECTOR_BUILDER_SERVER_API_PORT,
@@ -22,7 +25,6 @@ from literals import (
     REQUIRED_S3_PARAMETERS,
 )
 from log import log_event_handler
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from relations.airbyte_ui import AirbyteServerProvider
 from relations.minio import MinioRelation
 from relations.postgresql import PostgresqlRelation
@@ -35,7 +37,16 @@ logger = logging.getLogger(__name__)
 
 
 def get_pebble_layer(application_name, context):
-    return {
+    """Create pebble layer based on application.
+
+    Args:
+        application_name: Name of Airbyte application.
+        context: environment to include with the pebble plan.
+
+    Returns:
+        pebble plan dict.
+    """
+    pebble_layer = {
         "summary": "airbyte layer",
         "services": {
             application_name: {
@@ -50,23 +61,55 @@ def get_pebble_layer(application_name, context):
         },
     }
 
+    application_info = CONTAINERS[application_name]
+    if application_info is not None:
+        pebble_layer["services"][application_name].update(
+            {
+                "on-check-failure": {"up": "ignore"},
+            }
+        )
+        pebble_layer.update(
+            {
+                "checks": {
+                    "up": {
+                        "override": "replace",
+                        "period": "10s",
+                        "http": {
+                            "url": f"http://localhost:{application_info['port']}{application_info['health_endpoint']}"
+                        },
+                    }
+                }
+            }
+        )
+
+    return pebble_layer
+
 
 class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
-    """Charm the application."""
+    """Airbyte Server charm.
+
+    Attrs:
+        _state: used to store data that is persisted across invocations.
+        config_type: the charm structured config
+    """
 
     config_type = CharmConfig
 
     def __init__(self, *args):
+        """Construct.
+
+        Args:
+            args: Ignore.
+        """
         super().__init__(*args)
         self._state = State(self.app, lambda: self.model.get_relation("peer"))
 
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.peer_relation_changed, self._on_peer_relation_changed)
+        self.framework.observe(self.on.update_status, self._on_update_status)
 
         # Handle postgresql relation.
-        self.db = DatabaseRequires(
-            self, relation_name="db", database_name="airbyte-k8s_db", extra_user_roles="admin"
-        )
+        self.db = DatabaseRequires(self, relation_name="db", database_name="airbyte-k8s_db", extra_user_roles="admin")
         self.postgresql = PostgresqlRelation(self)
 
         self.minio = MinioRelation(self)
@@ -78,12 +121,16 @@ class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
         # Handle UI relation
         self.airbyte_ui = AirbyteServerProvider(self)
 
-        for container in list(CONTAINERS.keys()):
-            self.framework.observe(self.on[container].pebble_ready, self._on_pebble_ready)
+        for container_name in list(CONTAINERS.keys()):
+            self.framework.observe(self.on[container_name].pebble_ready, self._on_pebble_ready)
 
     @log_event_handler(logger)
     def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Handle pebble-ready event."""
+        """Handle pebble ready event.
+
+        Args:
+            event: The event triggered.
+        """
         self._update(event)
 
     @log_event_handler(logger)
@@ -94,6 +141,62 @@ class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
             event: The event triggered when the relation changed.
         """
         self._update(event)
+
+    @log_event_handler(logger)
+    def _on_update_status(self, event):
+        """Handle `update-status` events.
+
+        Args:
+            event: The `update-status` event triggered at intervals.
+        """
+        try:
+            self._validate()
+        except ValueError:
+            return
+
+        all_valid_plans = True
+        for container_name in list(CONTAINERS.keys()):
+            if not CONTAINERS[container_name]:
+                continue
+
+            container = self.unit.get_container(container_name)
+            valid_pebble_plan = self._validate_pebble_plan(container, container_name)
+            logger.info(f"validating pebble plan for {container_name}")
+            if not valid_pebble_plan:
+                logger.debug(f"failed to validate pebble plan for {container_name}, attempting creation again")
+                all_valid_plans = False
+                self._update(event)
+                continue
+
+            logger.info(f"performing up check for {container_name}")
+            check = container.get_check("up")
+            if check.status != CheckStatus.UP:
+                logger.error(f"check failed for {container_name}")
+                self.unit.status = MaintenanceStatus("Status check: DOWN")
+                return
+
+        if not all_valid_plans:
+            return
+
+        self.unit.status = ActiveStatus()
+        if self.unit.is_leader():
+            self.airbyte_ui._provide_server_status()
+
+    def _validate_pebble_plan(self, container, container_name):
+        """Validate pebble plan.
+
+        Args:
+            container: application container
+            container_name: name of container to check
+
+        Returns:
+            bool of pebble plan validity
+        """
+        try:
+            plan = container.get_plan().to_dict()
+            return bool(plan["services"][container_name]["on-check-failure"])
+        except (KeyError, ops.pebble.ConnectionError):
+            return False
 
     @log_event_handler(logger)
     def _on_config_changed(self, event):
@@ -150,7 +253,7 @@ class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
                 raise ValueError(f"s3:missing parameters {missing_params!r}")
 
     def _update(self, event):
-        """Update the Temporal server configuration and replan its execution.
+        """Update configuration and replan its execution.
 
         Args:
             event: The event triggered when the relation changed.
@@ -173,9 +276,7 @@ class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
                 s3_client.create_bucket_if_not_exists(bucket)
 
             logs_ttl = int(self.config["logs-ttl"])
-            s3_client.set_bucket_lifecycle_policy(
-                bucket=self.config[LOGS_BUCKET_CONFIG], ttl=logs_ttl
-            )
+            s3_client.set_bucket_lifecycle_policy(bucket_name=self.config[LOGS_BUCKET_CONFIG], ttl=logs_ttl)
         except (ClientError, ValueError) as e:
             logger.error(f"Error creating bucket and setting lifecycle policy: {e}")
             self.unit.status = BlockedStatus(f"failed to create buckets: {str(e)}")
@@ -184,31 +285,29 @@ class AirbyteK8SOperatorCharm(TypedCharmBase[CharmConfig]):
         env = create_env(self.model.name, self.app.name, self.config, self._state)
         self.model.unit.set_ports(INTERNAL_API_PORT, CONNECTOR_BUILDER_SERVER_API_PORT)
 
-        for service in list(CONTAINERS.keys()):
-            container = self.unit.get_container(service)
+        for container_name in list(CONTAINERS.keys()):
+            container = self.unit.get_container(container_name)
             if not container.can_connect():
                 event.defer()
                 return
 
-            if service == "airbyte-pod-sweeper":
+            if container_name == "airbyte-pod-sweeper":
                 script_path = Path(__file__).parent / "scripts/pod-sweeper.sh"
 
                 with open(script_path, "r") as file_source:
                     logger.info("pushing pod-sweeper script...")
                     container.push(
-                        f"/airbyte-app/bin/{service}",
+                        f"/airbyte-app/bin/{container_name}",
                         file_source,
                         make_dirs=True,
                         permissions=0o755,
                     )
 
-            pebble_layer = get_pebble_layer(service, env)
-            container.add_layer(service, pebble_layer, combine=True)
+            pebble_layer = get_pebble_layer(container_name, env)
+            container.add_layer(container_name, pebble_layer, combine=True)
             container.replan()
 
-        self.unit.status = ActiveStatus()
-        if self.unit.is_leader():
-            self.airbyte_ui._provide_server_status()
+        self.unit.status = MaintenanceStatus("replanning application")
 
 
 if __name__ == "__main__":  # pragma: nocover
--- src/charm_helpers.py
+++ src/charm_helpers.py
@@ -8,15 +8,25 @@ from urllib.parse import urlparse
 
 from literals import (
     AIRBYTE_API_PORT,
-    AIRBYTE_VERSION,
+    BASE_ENV,
     CONNECTOR_BUILDER_SERVER_API_PORT,
     INTERNAL_API_PORT,
-    WORKLOAD_API_PORT,
 )
 from structured_config import StorageType
 
 
 def create_env(model_name, app_name, config, state):
+    """Create set of environment variables for application.
+
+    Args:
+        model_name: Name of the juju model.
+        app_name: Name of the application.
+        config: Charm config.
+        state: Charm state.
+
+    Returns:
+        environment variables dict.
+    """
     db_conn = state.database_connection
 
     host = db_conn["host"]
@@ -24,20 +34,16 @@ def create_env(model_name, app_name, config, state):
     db_name = db_conn["dbname"]
     db_url = f"jdbc:postgresql://{host}:{port}/{db_name}"
 
-    # Some defaults are extracted from Helm chart: https://github.com/airbytehq/airbyte-platform/tree/v0.57.3/charts/airbyte
+    # Some defaults are extracted from Helm chart:
+    # https://github.com/airbytehq/airbyte-platform/tree/v0.60.0/charts/airbyte
     env = {
-        "API_URL": "/api/v1/",
-        "AIRBYTE_VERSION": AIRBYTE_VERSION,
-        "AIRBYTE_EDITION": "community",
-        "AUTO_DETECT_SCHEMA": "true",
+        **BASE_ENV,
         "DATABASE_URL": db_url,
         "DATABASE_USER": db_conn["user"],
         "DATABASE_PASSWORD": db_conn["password"],
         "DATABASE_DB": db_name,
         "DATABASE_HOST": host,
         "DATABASE_PORT": port,
-        "WORKSPACE_ROOT": "/workspace",
-        "CONFIG_ROOT": "/configs",
         "TEMPORAL_HOST": config["temporal-host"],
         "WORKER_LOGS_STORAGE_TYPE": config["storage-type"].value,
         "WORKER_STATE_STORAGE_TYPE": config["storage-type"].value,
@@ -47,32 +53,21 @@ def create_env(model_name, app_name, config, state):
         "STORAGE_BUCKET_STATE": config["storage-bucket-state"],
         "STORAGE_BUCKET_WORKLOAD_OUTPUT": config["storage-bucket-workload-output"],
         "STORAGE_BUCKET_ACTIVITY_PAYLOAD": config["storage-bucket-activity-payload"],
-        "CONFIGS_DATABASE_MINIMUM_FLYWAY_MIGRATION_VERSION": "0.40.23.002",
-        "JOBS_DATABASE_MINIMUM_FLYWAY_MIGRATION_VERSION": "0.40.26.001",
-        "MICRONAUT_ENVIRONMENTS": "control-plane",
-        "WORKERS_MICRONAUT_ENVIRONMENTS": "control-plane",
-        "CRON_MICRONAUT_ENVIRONMENTS": "control-plane",
-        "INTERNAL_API_HOST": f"localhost:{INTERNAL_API_PORT}",
-        "CONNECTOR_BUILDER_SERVER_API_HOST": f"localhost:{CONNECTOR_BUILDER_SERVER_API_PORT}",
-        "AIRBYTE_API_HOST": f"localhost:{AIRBYTE_API_PORT}",
-        "WORKLOAD_API_HOST": f"localhost:{WORKLOAD_API_PORT}",
-        "WORKLOAD_API_URL": f"localhost:{WORKLOAD_API_PORT}",
         "LOG_LEVEL": config["log-level"].value,
-        "MICROMETER_METRICS_ENABLED": "false",
-        "LAUNCHER_MICRONAUT_ENVIRONMENTS": "control-plane,oss",
-        "KEYCLOAK_INTERNAL_HOST": "localhost",
         "KEYCLOAK_DATABASE_URL": db_url + "?currentSchema=keycloak",
-        "WEBAPP_URL": "airbyte-ui-k8s:8080",
-        "WORKER_ENVIRONMENT": "kubernetes",
-        "SECRET_PERSISTENCE": "TESTING_CONFIG_DB_TABLE",
-        "SHOULD_RUN_NOTIFY_WORKFLOWS": "true",
-        "CONNECTOR_BUILDER_API_URL": "/connector-builder-api",
-        "TEMPORAL_WORKER_PORTS": "9001,9002,9003,9004,9005,9006,9007,9008,9009,9010,9011,9012,9013,9014,9015,9016,9017,9018,9019,9020,9021,9022,9023,9024,9025,9026,9027,9028,9029,9030",
         "JOB_KUBE_SERVICEACCOUNT": app_name,
         "JOB_KUBE_NAMESPACE": model_name,
         "RUNNING_TTL_MINUTES": config["pod-running-ttl-minutes"],
         "SUCCEEDED_TTL_MINUTES": config["pod-successful-ttl-minutes"],
         "UNSUCCESSFUL_TTL_MINUTES": config["pod-unsuccessful-ttl-minutes"],
+        "INTERNAL_API_HOST": f"{app_name}:{INTERNAL_API_PORT}",
+        "AIRBYTE_SERVER_HOST": f"{app_name}:{INTERNAL_API_PORT}",
+        "CONFIG_API_HOST": f"{app_name}:{INTERNAL_API_PORT}",
+        "CONNECTOR_BUILDER_SERVER_API_HOST": f"{app_name}:{CONNECTOR_BUILDER_SERVER_API_PORT}",
+        "CONNECTOR_BUILDER_API_HOST": f"{app_name}:{CONNECTOR_BUILDER_SERVER_API_PORT}",
+        "AIRBYTE_API_HOST": f"{app_name}:{AIRBYTE_API_PORT}/api/public",
+        "WEBAPP_URL": config["webapp-url"],
+        "AIRBYTE_URL": config["webapp-url"],
     }
 
     if config["storage-type"].value == StorageType.minio and state.minio:
@@ -146,25 +141,59 @@ def create_env(model_name, app_name, config, state):
 
 
 def _get_java_tool_options(http_proxy, https_proxy, no_proxy):
+    """Generate Java tool options for configuring HTTP and HTTPS proxies.
+
+    Args:
+        http_proxy: The HTTP proxy URL.
+        https_proxy: The HTTPS proxy URL.
+        no_proxy: A comma-separated string of hosts that should bypass the proxy.
+
+    Returns:
+        A string of Java tool options for the provided proxy settings.
+
+    Raises:
+        ValueError: If any provided proxy URL is invalid or cannot be parsed.
+    """
     options = ""
-    if http_proxy:
-        _, host, port = _split_url(http_proxy)
-        options += f"-Dhttp.proxyHost={host} -Dhttp.proxyPort={port}"
-    if https_proxy:
-        _, host, port = _split_url(https_proxy)
-        options += f" -Dhttps.proxyHost={host} -Dhttps.proxyPort={port}"
-    if no_proxy:
-        options += f" -Dhttp.nonProxyHosts={no_proxy.replace(',', '|')}"
+    try:
+        if http_proxy:
+            _, host, port = _split_url(http_proxy)
+            options += f"-Dhttp.proxyHost={host} -Dhttp.proxyPort={port}"
+        if https_proxy:
+            _, host, port = _split_url(https_proxy)
+            options += f" -Dhttps.proxyHost={host} -Dhttps.proxyPort={port}"
+        if no_proxy:
+            options += f" -Dhttp.nonProxyHosts={no_proxy.replace(',', '|')}"
+    except Exception as e:
+        raise ValueError(f"Invalid proxy URL: {e}") from e
 
     return options
 
 
 def _split_url(url):
-    parsed_url = urlparse(url)
-    protocol = parsed_url.scheme
-    host = parsed_url.hostname
-    port = parsed_url.port
-    return protocol, host, port
+    """Split the given URL into its components: protocol, host, and port.
+
+    Args:
+        url: The URL to be split.
+
+    Returns:
+        tuple: A tuple containing the protocol, host, and port.
+
+    Raises:
+        ValueError: If the URL is invalid or cannot be parsed.
+    """
+    try:
+        parsed_url = urlparse(url)
+        protocol = parsed_url.scheme
+        host = parsed_url.hostname
+        port = parsed_url.port
+
+        if not protocol or not host:
+            raise ValueError("Invalid URL: Protocol or host is missing")
+
+        return protocol, host, port
+    except Exception as e:
+        raise ValueError(f"Invalid URL: {e}") from e
 
 
 def construct_svc_endpoint(service_name, namespace, port, secure=False):
