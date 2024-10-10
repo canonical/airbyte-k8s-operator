c922b454690b7e02173a39a34d41ff7329b34c10
Ali Kelkawi <ali.kelkawi@canonical.com>
2024-10-10 13:59:56 +0300

    update charm code

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,13 +6,13 @@
 import logging
 from pathlib import Path
 
+import pytest
 import pytest_asyncio
 from helpers import (
     APP_NAME_AIRBYTE_SERVER,
     APP_NAME_TEMPORAL_ADMIN,
     APP_NAME_TEMPORAL_SERVER,
     create_default_namespace,
-    get_airbyte_charm_resources,
     perform_airbyte_integrations,
     perform_temporal_integrations,
     run_sample_workflow,
@@ -21,6 +21,14 @@ from pytest import FixtureRequest
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="module", name="charm_image")
+def charm_image_fixture(request: FixtureRequest) -> str:
+    """The OCI image for charm."""
+    charm_image = request.config.getoption("--airbyte-image")
+    assert charm_image, "--airbyte-image argument is required which should contain the name of the OCI image."
+    return charm_image
 
 
 @pytest_asyncio.fixture(scope="module", name="charm")
@@ -35,10 +43,11 @@ async def charm_fixture(request: FixtureRequest, ops_test: OpsTest) -> str | Pat
 
 
 @pytest_asyncio.fixture(name="deploy", scope="module")
-async def deploy(ops_test: OpsTest, charm: str):
+async def deploy(ops_test: OpsTest, charm: str, charm_image: str):
     """Test the app is up and running."""
     await ops_test.model.set_config({"update-status-hook-interval": "1m"})
-    resources = get_airbyte_charm_resources()
+    # resources = get_airbyte_charm_resources()
+    resources = {"airbyte-image": charm_image}
 
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME_AIRBYTE_SERVER, trust=True)
     await ops_test.model.deploy(
