1ef3d504e47aa63e08f75aba3d6dd43b42dd2672
onurmus <onurmusaolu@gmail.com>
2025-12-11 20:25:46 +0000

    fix for secret generation

--- generate-airbyte-auth-secrets.sh
+++ generate-airbyte-auth-secrets.sh
@@ -1,16 +1,16 @@
 # Pick namespace used by your model (default in your setup is "airbyte")
-NS=airbyte
+NS=airbyte-2
 
 # Generate values
 ID=$(uuidgen)
 SECRET=$(openssl rand -hex 32)
 
 # Create the secret with the expected keys
-kubectl -n "$NS" create secret generic airbyte-auth-secrets \
+microk8s.kubectl -n "$NS" create secret generic airbyte-auth-secrets \
   --from-literal=dataplane-client-id="$ID" \
   --from-literal=dataplane-client-secret="$SECRET"
 
 # Verify
-kubectl -n "$NS" get secret airbyte-auth-secrets -o yaml
+microk8s.kubectl -n "$NS" get secret airbyte-auth-secrets -o yaml
 
 juju trust airbyte-k8s --scope=cluster
